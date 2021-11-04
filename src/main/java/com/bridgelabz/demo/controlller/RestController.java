41b2d00187611102f6cad0795f033ed0d06df835
Prashanth <imprash4u@gmail.com>
2021-11-04 17:29:40 +0530

    Added: Rest Call to Show Hello +name+ from bridgelabz using GET request method

--- src/main/java/com/bridgelabz/demo/controlller/RestController.java
+++ src/main/java/com/bridgelabz/demo/controlller/RestController.java
@@ -2,6 +2,8 @@ package com.bridgelabz.demo.controlller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hello")
@@ -9,5 +11,10 @@ public class RestController {
     @RequestMapping(value = {"", "/", "home"})
     public String sayHello() {
         return "Hello From Bridgelabz..";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value= "name")String name){
+        return "Hello "+ name +" From Bridgelabz";
     }
 }
